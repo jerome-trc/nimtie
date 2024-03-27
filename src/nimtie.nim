fe70f89d3727299b7540420ab08f57d7d4d63a03
jerome-trc <46534306+jerome-trc@users.noreply.github.com>
2024-03-27 14:20:59 -0400

    Break configuration types out into their own module

--- src/nimtie.nim
+++ src/nimtie.nim
@@ -1,56 +1,8 @@
-import nimtie/internal, nimtie/languages/c, nimtie/languages/cpp,
-    nimtie/languages/nim, nimtie/languages/node, nimtie/languages/python,
-    nimtie/languages/zig, macros, strformat
+import std/[macros, strformat]
 
-type
-  BraceStyle* {.pure.} = enum
-    sameLine
-    newLine
-
-  RenameRule* {.pure.} = enum
-    none
-    geckoCase
-    lowerCase
-    upperCase
-    pascalCase
-    camelCase
-    snakeCase
-    upperSnakeCase
-
-  Target* {.pure.} = enum
-    ## What languages should bindings be generated for?
-    c
-    cxx
-    js
-    python
-    zig
-  Targets* = set[Target]
-    ## What languages should bindings be generated for?
-
-  CConfig* = object
-    ## Configuration for generated C bindings.
-    ## Note that some of these settings also apply to generated C++ bindings.
-    braceStyle*: BraceStyle = BraceStyle.sameLine
-    fixedWidthInt*: bool = true ## \
-      ## Whether to use `[u]int##_t` types instead of `int`, `long`, etc.
-    includeGuard*: string = "" ## \
-      ## Note that this is not mutually-exclusive with `pragmaOnce`.
-    includes*: seq[string] = @[] ## \
-      ## Each string is written as-is, so quotes or angle brackets must be included.
-    pragmaOnce*: bool = true ## \
-      ## Note that this is not mutually-exclusive with `includeGuard`.
-
-  Config* = object
-    ## Passed to `proc writeFiles`_.
-    directory*: string = "."
-    filename*: string = "mylib"
-    procPrefix*: string = "" ## \
-      ## Prepended to the name of every generated function binding.
-    structPrefix*: string = "" ## \
-      ## Prepended to the name of every generated struct declaration.
-    targets*: Targets = {} ## \
-      ## What languages should bindings be generated for?
-    cCfg*: CConfig
+import nimtie/config
+import nimtie/internal
+import nimtie/languages/[c, cpp, nim, node, python, zig]
 
 template discard2(f: untyped): untyped =
   when(compiles do: discard f):
